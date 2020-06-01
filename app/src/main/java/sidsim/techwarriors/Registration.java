09cbc3ed7d5a113635687ceddae02dd2594aaf60
siddharth1597 <sidd15597@gmail.com>
2020-06-02 02:46:32 +0530

    Project Repo for Hospital

---
+++ app/src/main/java/sidsim/techwarriors/Registration.java
@@ -0,0 +1,13 @@
+package sidsim.techwarriors;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
